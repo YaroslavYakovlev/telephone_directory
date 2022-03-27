7924d39abe40a91b7f27383a60d79be68e7d424b
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-03-27 17:52:24 +0500

    изменил формат запроса

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,6 @@
 #include <map>
 #include <string>
 
-struct command {
-  std::string add = "add";
-  std::string find = "find";
-};
-
 std::map<std::string, std::string> directory;
 
 void telephone_directory() {
@@ -22,35 +17,33 @@ void fill_telephone_directory(std::string telephone, std::string person) {
 
 int main() {
   std::cout << "Telephone directory" << std::endl;
-  command cmd;
   std::string data;
   std::string phone;
   std::string person;
-  std::string action;
+  bool access = true;
   telephone_directory();
-  std::cout << "Enter command (add/find)" << std::endl;
-  std::cin >> action;
-  if (action == cmd.find) {
-    std::cout << "Enter data" << std::endl;
-    std::cin >> data;
+  std::cout << "Enter data" << std::endl;
+  std::cin >> data;
+  for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); it++) {
+    if (it->first == data) {
+      std::cout << "Telephone - " << it->first << " Person " << it->second << std::endl;
+      std::cout << std::endl;
+      access = false;
+    } else if (it->second == data) {
+      std::cout << "Telephone - " << it->first << " Person " << it->second << std::endl;
+      std::cout << std::endl;
+      access = false;
+    }
+  }
+  if (access == true)
     for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); it++) {
-      if (it->first == data) {
-        std::cout << "Telephone - " << it->first << " Person " << it->second << std::endl;
-        std::cout << std::endl;
-      } else if (it->second == data) {
-        std::cout << "Telephone - " << it->first << " Person " << it->second << std::endl;
-        std::cout << std::endl;
+      if (it->first != data) {
+        std::cout << "Enter person:" << std::endl;
+        std::cin >> person;
+        fill_telephone_directory(data, person);
+        break;
       }
     }
-  } else if (action == cmd.add) {
-    std::cout << "Enter telephone:" << std::endl;
-    std::cin >> phone;
-    std::cout << "Enter person:" << std::endl;
-    std::cin >> person;
-    fill_telephone_directory(phone, person);
-  } else {
-    std::cout << "Error command" << std::endl;
-  }
 
   for (std::map<std::string, std::string>::iterator it = directory.begin(); it != directory.end(); it++) {
     std::cout << "Telephone - " << it->first << " Person " << it->second << std::endl;
